171ec88f0de84ca0f562f1104c5fdb1126b9ad51
kazaraal <araalkaz@gmail.com>
2026-02-13 12:02:00 +0000

    Removed unwanted folders

--- jenkins-setup/namespace.tf
+++
@@ -1,9 +0,0 @@
-resource "kubernetes_namespace_v1" "ci_cd" {
-  metadata {
-    name = "ci-cd-${var.environment}"
-    labels = {
-      name        = "ci-cd"
-      environment = var.environment
-    }
-  }
-}
