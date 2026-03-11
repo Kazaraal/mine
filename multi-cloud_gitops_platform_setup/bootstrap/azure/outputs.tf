f21a637f53473f3e8d51804ba8bc4b75ee67544b
kazaraal <araalkaz@gmail.com>
2026-03-11 05:54:00 +0000

    Edited bootstrap files

---
+++ multi-cloud_gitops_platform_setup/bootstrap/azure/outputs.tf
@@ -0,0 +1,17 @@
+# multi-cloud_gitops_platform_setup/bootstrap/azure/outputs.tf
+output "resource_group_name" {
+  value = azurerm_resource_group.state_rg.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.state_sa.name
+}
+
+output "container_name" {
+  value = azurerm_storage_container.state_container.name
+}
+
+output "primary_access_key" {
+  value     = azurerm_storage_account.state_sa.primary_access_key
+  sensitive = true
+}
