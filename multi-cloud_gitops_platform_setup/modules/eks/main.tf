b27f4a64953d5a9bee5619b27701228bb11329e4
kazaraal <araalkaz@gmail.com>
2026-03-15 15:55:51 +0000

    Edited placeholders in the eks module

---
+++ multi-cloud_gitops_platform_setup/modules/eks/main.tf
@@ -0,0 +1,10 @@
+# multi-cloud_gitops_platform_setup/modules/eks/main.tf
+
+resource "null_resource" "placeholder" {
+  triggers = {
+    always_run = timestamp()
+  }
+
+  data "aws_availability_zones" "available" {
+    state = "available"
+  }
---
+++ multi-cloud_gitops_platform_setup/modules/eks/outputs.tf
@@ -0,0 +1,16 @@
+# multi-cloud_gitops_platform_setup/modules/eks/outputs.tf
+
+output "cluster_id" {
+  description   = "EKS cluster ID"
+  value         = aws_eks_cluster.this.id
+}
+
+output "cluster_endpoint" {
+  description   = "Endpoint for EKS control plane"
+  value         = aws_eks_cluster.this.endpoint
+}
+
+output "kubeconfig_certificate_authority_data" {
+  description   = "Base64 encoded certificate data required to communicate with the cluster"
+  value         = aws_eks_cluster.this.certificate_authority[9].data
+}
